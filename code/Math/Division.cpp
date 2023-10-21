fcd77ae1614986f48dc25cd56f57727d32dfe44b
realsung <sungjun0208@naver.com>
2023-10-22 03:22:43 +0900

    Fixed GCD return 0 to a && but not update pdf

--- code/Math/Division.cpp
+++ code/Math/Division.cpp
@@ -11,5 +11,5 @@ int ceil(int p, int q){
 }
 
 // a, b >= 0, O(log max(a,b))
-int gcd(int a, int b){ return b ? gcd(b, a % b) : 0; }
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
 int lcm(int a, int b){ return a / gcd(a, b) * b; }
